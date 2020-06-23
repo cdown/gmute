f0b22708dafc5e386994bbd9dd169b55f32516b8
Chris Down <chris@chrisdown.name>
2020-06-23 19:48:40 +0100

    Mute all In-Reply-To threads, as well
    This is useful so you can tag cover letters to get the whole thread.

--- gmute.py
+++ gmute.py
@@ -79,31 +79,48 @@ def print_mid(mid, message, **kwargs):
 def login(user, password, dry_run):
     print("Logging in...")
     icheck(cur.login(user, password))
-    icheck(cur.select('"[Gmail]/All Mail"', readonly=dry_run))
+    icheck(cur.select("INBOX", readonly=dry_run))
 
 
 def mark(mid, dry_run):
+    base_uids_to_mute = set()
+
     print_mid(mid, "Searching for UID")
     initial_uid = icheck(cur.search(None, "(HEADER Message-ID %s)" % mid))
     if not initial_uid:
         print_mid(mid, "No such message", file=sys.stderr)
         return 1
-    print_mid(mid, "Got UID {}".format(initial_uid))
+    base_uids_to_mute.add(initial_uid)
+    print_mid(mid, "Got initial UID {}".format(initial_uid))
 
-    print_mid(mid, "Fetching thread ID")
-    data = icheck(cur.fetch(str(initial_uid), "(X-GM-THRID)"))
-    thread_id = data.split()[-1][:-1]
+    # If this is a patch cover letter, all of the rest are going to have
+    # different X-GM-THRIDs
+    print_mid(mid, "Looking for In-Reply-Tos")
+    data = icheck(cur.search(None, "(HEADER In-Reply-To {})".format(mid)))
+    if data:
+        child_uids = data.split()
+        base_uids_to_mute.update(child_uids)
+        print_mid(mid, "Got child UIDs {}".format(child_uids))
 
-    print_mid(mid, "Looking for all mails with thread ID {}".format(thread_id))
-    data = icheck(cur.search(None, "(X-GM-THRID {})".format(thread_id)))
-    all_uids = data.split()
+    thread_ids = set()
+    for uid in base_uids_to_mute:
+        print_mid(mid, "Fetching thread ID for {}".format(uid))
+        data = icheck(cur.fetch(str(uid), "(X-GM-THRID)"))
+        thread_ids.add(data.split()[-1][:-1])
 
-    if dry_run:
-        print_mid(mid, "Would tag {} mails, but in dry run mode".format(len(all_uids)))
-    else:
-        print_mid(mid, "Found {} mails to tag. Tagging...".format(len(all_uids)))
-        for uid in all_uids:
-            icheck(cur.store(uid, "+X-GM-LABELS", r"(\Muted)"))
+    for thread_id in thread_ids:
+        print_mid(mid, "Looking for all mails with thread ID {}".format(thread_id))
+        data = icheck(cur.search(None, "(X-GM-THRID {})".format(thread_id)))
+        all_uids = data.split()
+
+        if dry_run:
+            print_mid(
+                mid, "Would tag {} mails, but in dry run mode".format(len(all_uids))
+            )
+        else:
+            print_mid(mid, "Found {} mails to tag. Tagging...".format(len(all_uids)))
+            for uid in all_uids:
+                icheck(cur.store(uid, "+X-GM-LABELS", r"(\Muted)"))
 
     print_mid(mid, "Complete for this Message-ID.")
 
